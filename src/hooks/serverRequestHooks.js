ad07a91e7329fda9d8f14f60f98c8472187b9492
Антон <tohanknv@yandex.ru>
2022-01-29 19:03:09 +0300

    server path fix

--- src/hooks/serverRequestHooks.js
+++ src/hooks/serverRequestHooks.js
@@ -140,7 +140,7 @@ export const useRequestHooks = () => {
 
     const sendPostRequest = async (path, dataName, data, userData, extraArgument) => {
         try {
-            const request = await fetch(`https://mycollection-app.herokuapp.com/api/${path}`, getRequestOptions(dataName, data))
+            const request = await fetch(`https://mycollection-server.herokuapp.com/api/${path}`, getRequestOptions(dataName, data))
             const response = await request.json();
             console.log(response)
             console.log(response.message && response.message.includes('Error')?true:false)
@@ -182,7 +182,7 @@ export const useRequestHooks = () => {
 
     const sendGetRequest = async (path) => {
       try {
-        const request = await fetch(`https://mycollection-app.herokuapp.com/api/${path}`)
+        const request = await fetch(`https://mycollection-server.herokuapp.com/api/${path}`)
         const response = await request.json();
         if (response.message && response.message.includes('Error')){
           console.log(response.message)
